62f0cd84d9024220e02a78b4d48837b8f4090960
Lucas <lumuryllo86@gmail.com>
2023-05-21 17:54:06 -0300

    feat: Add new component and does de request to API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,37 @@
+import { useState, useEffe, useEffect } from "react";
+
+const moviesUrl = import.meta.env.VITE_API
+const apiKey = import.meta.env.VITE_API_KEY
 
 
 const Home = () => {
-    return <div>Home</div>
+    const [topMovies, setTopMovies] = useState([])
+
+    const getTopRatedMovies = async (url) => {
+        const res = await fetch(url)
+        const data = await res.json()
+
+        setTopMovies(data.results)
+    }
+
+    useEffect(() => {
+        const topRatedUrl = `${moviesUrl}top_rated?${apiKey}`
+
+
+        getTopRatedMovies(topRatedUrl)
+    }, [])
+
+    return (
+        <div className="container">
+        <h2 className="title">
+            Melhores Filmes:
+        </h2>
+        <div className="movies-container">
+            {topMovies.length === 0 && <p>carregando...</p>}
+        {topMovies.length > 0 && topMovies.map((movie) => <p>{movie.title}</p>)}
+        </div>
+    </div>
+    )
 };
 
 export default Home;
